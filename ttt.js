3ea829e556ea38d6b9b095ad9c8372e6c08b0794
Ben Fetterman <bjfett24@gmail.com>
2025-05-29 17:01:44 -0400

    make the game able to play multiple rounds
    Still having trouble with resetting the game due
    to my lack of understanding of closure and scoping

--- ttt.js
+++ ttt.js
@@ -4,12 +4,19 @@ const gameBoard = (function () {
     let board = [' ', ' ', ' ',
                  ' ', ' ', ' ',
                  ' ', ' ', ' '];
+
+    const resetBoard = () => {
+        board = [' ', ' ', ' ',
+                 ' ', ' ', ' ',
+                 ' ', ' ', ' '];
+    }
+
     const addMark = (mark, location) => {
             board.splice(location, 1, mark);
             console.log(board);
         }
     
-    return { addMark, board};
+    return { addMark, board, resetBoard };
 })();
 
 //players must also be stored in objects
@@ -18,24 +25,21 @@ const gameBoard = (function () {
 
 const game = (function () {
     let gameStatus = undefined;
+    const gameSquares = document.querySelectorAll('.gameSquare');
     let round = 0;
-    const playTurn = () => {
-        if (round % 2 === 0) {
-            if (gameBoard.board.at(location) === ' ') {
-            gameBoard.addMark('X', location);
-            ++round;
-            } else {
-                playTurn()
-            }
+
+    const toggleGameStatus = () => {
+        if (gameStatus === undefined) {
+            gameStatus = true;
         } else {
-            if (gameBoard.board.at(location) === ' ') {
-                gameBoard.addMark('X', location);
-                ++round;
-                } else {
-                    playTurn()
-                }
+            gameStatus = undefined
         }
     }
+
+    const resetRounds = () => {
+        round = 0;
+    }
+
     const checkGame = () => {
         if ((gameBoard.board[0] === 'X' && gameBoard.board[1] === 'X' && gameBoard.board[2] === 'X') 
             || (gameBoard.board[3] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[5] === 'X')
@@ -45,7 +49,7 @@ const game = (function () {
             || (gameBoard.board[2] === 'X' && gameBoard.board[5] === 'X' && gameBoard.board[8] === 'X')
             || (gameBoard.board[0] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[8] === 'X')
             || (gameBoard.board[2] === 'X' && gameBoard.board[4] === 'X' && gameBoard.board[6] === 'X')) {
-                gameStatus = true;
+                toggleGameStatus();
                 return 'X'
             } else if ((gameBoard.board[0] === 'O' && gameBoard.board[1] === 'O' && gameBoard.board[2] === 'O') 
                 || (gameBoard.board[3] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[5] === 'O')
@@ -55,44 +59,67 @@ const game = (function () {
                 || (gameBoard.board[2] === 'O' && gameBoard.board[5] === 'O' && gameBoard.board[8] === 'O')
                 || (gameBoard.board[0] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[8] === 'O')
                 || (gameBoard.board[2] === 'O' && gameBoard.board[4] === 'O' && gameBoard.board[6] === 'O')) {
-                    gameStatus = true;
+                    toggleGameStatus();
                     return 'O'
-            } else if (!(' ' in gameBoard.board)) {
-                gameStatus = true;
+            } else if (!gameBoard.board.includes(' ')) {
+                toggleGameStatus();
                 return 'Draw'
-            }
+            } else {return undefined}
     }
     const playGame = () => {
-        while (gameStatus === undefined) {
-            console.log(gameBoard.board);
-            const winner = checkGame();
+        playGameButton.textContent = 'Reset';
+        playGameButton.style.backgroundColor = 'red'
+        
+
+
+        for (square of gameSquares) {
+            square.addEventListener('click', function() {
+                if (gameStatus === undefined) {
+                    if (!(this.textContent == 'X' || this.textContent == 'O')) {
+                        if (round % 2 === 0) {
+                            this.textContent = 'X';
+                            gameBoard.addMark('X', +this.id);
+                            ++round;
+                        } else {
+                            this.textContent = 'O'
+                            gameBoard.addMark('O', +this.id);
+                            ++round;
+                        }
+                    }
+                    let winner = checkGame()
+                    if (winner != undefined) {
+                        console.log(winner);
+                    }
+                }
+        })
+
         }
-        alert('Winner is ', winner, "'s")
     }
-    return { playGame, checkGame, playTurn, round, gameStatus };
+    return { playGame, checkGame, gameSquares, toggleGameStatus, resetRounds};
 })();
 
 
-const playGameButton = document.querySelector('.playGame');
-playGameButton.addEventListener('click', () => {
-    playGameButton.textContent = 'Restart';
-    game.playGame
-})
+const playGameButton = document.querySelector('.action');
+playGameButton.addEventListener('click', function() {
+    if (playGameButton.textContent === 'Play Game') {
+        game.resetRounds();
+        gameBoard.resetBoard();
+        game.playGame()
+    } else {
+        resetGame()
+    }
+});
 
-const gameSquares = document.querySelectorAll('.gameSquare');
+function resetGame() {
+    playGameButton.textContent = 'Play Game';
+    playGameButton.style.backgroundColor = 'green';
+    game.toggleGameStatus();
+    for (square of game.gameSquares) {
+        square.textContent = '';
+    }    
+    
 
-for (square of gameSquares) {
-    square.addEventListener('click', function() {
-        if (!(this.textContent == 'X' || this.textContent == 'O')) {
-            if (game.round % 2 === 0) {
-                this.textContent = 'X';
-                gameBoard.addMark('X', +this.id);
-                ++game.round;
-            } else {
-                this.textContent = 'O'
-                gameBoard.addMark('O', +this.id);
-                ++game.round;
-            }
-        }
-    })
 }
+
+
+    
